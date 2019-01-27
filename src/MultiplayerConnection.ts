728d37d542f4a7f9dd0c9004c68f048831265c5a
Dakota Larson <dakotalarson1996@gmail.com>
2019-01-27 09:51:08 -0600

    protocol update

--- src/MultiplayerConnection.ts
+++ src/MultiplayerConnection.ts
@@ -7,7 +7,7 @@ import PacketSender from "./PacketSender";
 
 export default class MultiplayerConnection extends Component {
 
-    private static readonly PROTOCOL = "battletanks-1";
+    private static readonly PROTOCOL = "battletanks-2";
 
     private static readonly CLIENT_OUTDATED_CODE = 4001;
     private static readonly SERVER_OUTDATED_CODE = 4002;
