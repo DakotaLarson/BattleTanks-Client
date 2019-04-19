4ad45e5b38bcc9b428200a0cf87a90c565b4ccbb
Dakota Larson <dakotalarson1996@gmail.com>
2019-04-19 07:42:08 -0500

    start on side panel

---
+++ src/main_menu/SidePanel.ts
@@ -0,0 +1,16 @@
+import ChildComponent from "../component/ChildComponent";
+
+export default class SidePanel extends ChildComponent {
+
+    constructor() {
+        super();
+    }
+
+    public enable() {
+
+    }
+
+    public disable() {
+
+    }
+}
